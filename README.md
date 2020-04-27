a9707d9e312dc2d5ff04acb5021f02bac4931970
Victor Charpenay <victor.charpenay@fau.de>
2020-04-27 20:12:11 +0200

    added initial HTML doc for CoAP vocab

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Each commit here will sync it to the master, which will expose the content to http://w3c.github.io/wot-binding-templates/.
 
 To make contributions, please provide pull-requests to the html file, see [github help](https://help.github.com/articles/using-pull-requests/).
+
+Some HTML files are automatically rendered from RDF sources. To render them, install Node.js (if necessary) and run:
+```sh
+$ npm i # to do once to install dependencies
+$ node render.js
+```
---
+++ render.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+let sttl = require('sttl');
+let urdf = require('urdf');
+
+const tpl = fs.readFileSync('ontology/templates.rq', 'utf-8');
+const main = 'http://w3c.github.io/wot-binding-templates/ontology#main';
+
+sttl.register(tpl);
+sttl.connect(q => {
+    return urdf.query(q)
+    .then(b => ({
+        results: {
+            bindings: b
+        }
+    }));
+});
+
+const src = [
+    'ontology/coap.ttl'
+    // TODO mqtt.ttl
+];
+
+src.forEach(f => {
+    let ttl = fs.readFileSync(f, 'UTF-8');
+    
+    urdf.load(ttl, { format: 'text/turtle' })
+
+    .then(() => sttl.callTemplate(main, ttl))
+
+    .then(html => fs.writeFileSync(f.replace('.ttl', '.html'), html))
+
+    .catch((e) => {
+        console.error('Error while rendering ' + f + ': ' + e);
+    });
+});

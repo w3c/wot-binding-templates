d5236c6b1c1672ec8ceb28a657f3d1a18281aedd
reluc <relu.cri@gmail.com>
2021-02-17 15:11:22 +0100

    improve modbus document generation

--- render.js
+++ render.js
@@ -3,9 +3,11 @@ let sttl = require('sttl');
 let urdf = require('urdf');
 
 const tpl = fs.readFileSync('ontology/templates.rq', 'utf-8');
+const modbusTpl = fs.readFileSync('ontology/modbus.template.rq', 'utf-8');
 const main = 'http://w3c.github.io/wot-binding-templates/ontology#main';
 
 sttl.register(tpl);
+sttl.register(modbusTpl);
 sttl.connect(q => {
     return urdf.query(q)
     .then(b => ({
@@ -16,19 +18,20 @@ sttl.connect(q => {
 });
 
 const src = [
-    'ontology/coap.ttl',
-    'ontology/mqtt.ttl',
-    'ontology/modbus.ttl'
+    ['ontology/coap.ttl',main],
+    ['ontology/mqtt.ttl',main],
+    ['ontology/modbus.ttl',"http://w3c.github.io/wot-binding-templates/ontology#modbus"]
 ];
 
-src.reduce((p, f) => {
+src.reduce((p, src) => {
+    const f = src[0]
+    const template = src[1]
     let ttl = fs.readFileSync(f, 'UTF-8');
-    
     return p.then(() => urdf.clear())
 
     .then(() => urdf.load(ttl, { format: 'text/turtle' }))
 
-    .then(() => sttl.callTemplate(main, ttl))
+    .then(() => sttl.callTemplate(template, ttl))
 
     .then(html => fs.writeFileSync(f.replace('.ttl', '.html'), html))
 

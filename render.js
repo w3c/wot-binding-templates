6628b02e2869e4d13cd95444d0d81d127581412d
reluc <relu.cri@gmail.com>
2021-03-08 12:35:44 +0100

    chore: move modbus spec to its own file

--- render.js
+++ render.js
@@ -3,11 +3,8 @@ let sttl = require('sttl');
 let urdf = require('urdf');
 
 const tpl = fs.readFileSync('ontology/templates.rq', 'utf-8');
-const modbusTpl = fs.readFileSync('ontology/modbus.template.rq', 'utf-8');
-const main = 'http://w3c.github.io/wot-binding-templates/ontology#main';
 
 sttl.register(tpl);
-sttl.register(modbusTpl);
 sttl.connect(q => {
     return urdf.query(q)
     .then(b => ({
@@ -17,25 +14,49 @@ sttl.connect(q => {
     }));
 });
 
-const src = [
-    ['ontology/coap.ttl',main],
-    ['ontology/mqtt.ttl',main],
-    ['ontology/modbus.ttl',"http://w3c.github.io/wot-binding-templates/ontology#modbus"]
+const ontologies = [
+    'ontology/coap.ttl',
+    'ontology/mqtt.ttl',
+    'ontology/modbus.ttl'
 ];
 
-src.reduce((p, src) => {
-    const f = src[0]
-    const template = src[1]
-    let ttl = fs.readFileSync(f, 'UTF-8');
-    return p.then(() => urdf.clear())
 
-    .then(() => urdf.load(ttl, { format: 'text/turtle' }))
-
-    .then(() => sttl.callTemplate(template, ttl))
-
-    .then(html => fs.writeFileSync(f.replace('.ttl', '.html'), html))
-
-    .catch((e) => {
-        console.error('Error while rendering ' + f + ': ' + e);
-    });
+console.log("Rendering ontology documentation...");
+const promiseChain = ontologies.reduce((p, src) => {
+    const ontologyFile = src
+    const templateURI = 'http://w3c.github.io/wot-binding-templates/ontology#main'
+    return p.then( _ => render(ontologyFile,templateURI,ontologyFile.replace('.ttl', '.html'))).then(() => urdf.clear())
 }, Promise.resolve());
+
+
+console.log("Rendering WoT binding documentation...");
+
+const mappings = [
+    ['bindings/modbus/mapping.ttl','ontology/modbus.ttl','http://w3c.github.io/wot-binding-templates/mappings#modbus'],
+];
+
+
+const modbusTemplate = fs.readFileSync('bindings/modbus/template.sparql', 'utf-8');
+sttl.register(modbusTemplate);
+
+mappings.reduce((p, src) => {
+    const ontologyFile = src[0]
+    const baseOntologyFile = src[1]
+    const templateURI = src[2]
+    let base = fs.readFileSync( baseOntologyFile, 'UTF-8');
+    return p.then( _ => urdf.load(base,{ format: 'text/turtle' }))
+        .then( _ => render(ontologyFile,templateURI,ontologyFile.replace('mapping.ttl', 'index.html')))
+        .then(() => urdf.clear());
+},promiseChain);
+
+function render(ontologyFile,templateURI,output) {
+    let ttl = fs.readFileSync(ontologyFile, 'UTF-8');
+    return Promise.resolve()
+    .then(() => urdf.load(ttl, { format: 'text/turtle' }))
+    .then(() => sttl.callTemplate(templateURI, ttl))
+    .then(html => fs.writeFileSync(output, html))
+    .then(_ => console.log("File ",output,"generated"))
+    .catch((e) => {
+        console.error('Error while rendering ' + ontologyFile + ': ' + e);
+    });
+}
